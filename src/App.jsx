fbea09f856fa6ee30427de0db4319d63f53bbad8
Ngoc Long <dangngoclong28012002@gmail.com>
2025-04-08 20:21:59 +0700

    Initial commit again

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/common/Home'
+import Shop from './components/common/Shop'
+
+function App() {
+  
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/shop' element={<Shop/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
