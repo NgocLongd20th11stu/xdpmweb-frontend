9ed74e93bdf18fbbd31409807ade15deb1143135
Ngoc Long <dangngoclong28012002@gmail.com>
2025-04-15 16:59:13 +0700

    Cập nhật giao diện mới

---
+++ src/components/common/LatestProducts.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import ProductImg1 from '../../assest/products/t-shirt/naturalReserve.png';
+import ProductImg2 from '../../assest/products/t-shirt/aothun_OversizedFit.png';
+
+// Sản phẩm mới nhất
+const LatestProducts = () => {
+  return (
+    <section className='section-2 py-5'>
+        <div className='container'>
+            <h2>SẢN PHẨM MỚI</h2>
+            <div className='row mt-4'>
+                <div className='col-md-3'>
+                    <div className='product card border-0'>
+                        <div className='card-img'> 
+                            <img src={ProductImg1} alt="" className='w-100'/>
+                        </div>
+                        <div className='card-body pt-3'>
+                            <a href="">Áo thun Natural Reserve</a>
+                            <div className='price'>
+                                300.000 VND <span className='text-decoration-line-through'>450.000VND</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-3'>
+                    <div className='product card border-0'>
+                        <div className='card-img'> 
+                            <img src={ProductImg2} alt="" className='w-100'/>
+                        </div>
+                        <div className='card-body pt-3'>
+                            <a href="">Áo thun So Fresh So Clean</a>
+                            <div className='price'>
+                                250.000 VND <span className='text-decoration-line-through'>450.000VND</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default LatestProducts
