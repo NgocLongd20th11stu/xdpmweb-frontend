3937cc4fa6432279b892ed1756c10a9645b18777
Ngoc Long <dangngoclong28012002@gmail.com>
2026-05-07 07:38:40 +0700

    Fix: Cấu hình động apiURL

--- src/components/common/http.jsx
+++ src/components/common/http.jsx
@@ -1,12 +1,13 @@
-export const apiURL = 'http://localhost:8000/api'
-//export const apiURL = 'https://xdpmweb-backend.onrender.com/api'
+export const apiURL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
 
-export const adminToken = () =>{
-    const data = JSON.parse(localStorage.getItem('adminInfo'))
-    return data.token;
-}
 
-export const userToken = () =>{
-    const data = JSON.parse(localStorage.getItem('userInfo'))
-    return data.token;
-}
+export const adminToken = () => {
+  const data = JSON.parse(localStorage.getItem("adminInfo"));
+  return data.token;
+};
+
+export const userToken = () => {
+  const data = JSON.parse(localStorage.getItem("userInfo"));
+  return data.token;
+};
