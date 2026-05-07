fae175d30f7708a5dd150504e09c5b2029467e45
Ngoc Long <dangngoclong28012002@gmail.com>
2026-05-07 07:43:48 +0700

    Fix: Cấu hình file http

--- src/components/common/http.jsx
+++ src/components/common/http.jsx
@@ -4,10 +4,10 @@ export const apiURL =
 
 export const adminToken = () => {
   const data = JSON.parse(localStorage.getItem("adminInfo"));
-  return data.token;
+  return data?.token;
 };
 
 export const userToken = () => {
   const data = JSON.parse(localStorage.getItem("userInfo"));
-  return data.token;
+  return data?.token;
 };
